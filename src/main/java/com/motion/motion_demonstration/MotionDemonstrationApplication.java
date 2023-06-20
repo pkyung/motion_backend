fc2faa309180003b817480325a182a29dd810eb3
EunKyung Lee <2dmsrud2002@naver.com>
2023-06-21 00:40:36 +0900

    project setting

---
+++ src/main/java/com/motion/motion_demonstration/MotionDemonstrationApplication.java
@@ -0,0 +1,13 @@
+package com.motion.motion_demonstration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotionDemonstrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotionDemonstrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/motion/motion_demonstration/MotionDemonstrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motion.motion_demonstration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotionDemonstrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
