ef8bf55c00480654c0bcf2645c988e297a1b46e3
EunKyung Lee <2dmsrud2002@naver.com>
2023-06-22 23:26:25 +0900

    request dto add

---
+++ src/main/java/com/motion/motion_demonstration/dto/OneRequestDto.java
@@ -0,0 +1,13 @@
+package com.motion.motion_demonstration.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OneRequestDto {
+
+    private String name;
+}
---
+++ src/main/java/com/motion/motion_demonstration/dto/TwoRequestDto.java
@@ -0,0 +1,21 @@
+package com.motion.motion_demonstration.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TwoRequestDto {
+
+    private String caltime;
+    private String time;
+    private String location;
+    private String distance;
+    private String wind;
+    private String stdnoise;
+    private String noise;
+    private String name;
+
+}
