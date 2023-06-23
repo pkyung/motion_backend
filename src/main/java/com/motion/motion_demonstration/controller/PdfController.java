b39149f854466075eb2760532d5a36c9309feb33
EunKyung Lee <2dmsrud2002@naver.com>
2023-06-24 00:00:52 +0900

    img download complete

--- src/main/java/com/motion/motion_demonstration/controller/PdfController.java
+++ src/main/java/com/motion/motion_demonstration/controller/PdfController.java
@@ -6,11 +6,20 @@ import com.motion.motion_demonstration.dto.TwoRequestDto;
 import com.motion.motion_demonstration.service.PdfService;
 import com.motion.motion_demonstration.service.PdfToImgService;
 import lombok.AllArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 @AllArgsConstructor
@@ -20,14 +29,38 @@ public class PdfController {
     private final PdfToImgService pdfToImgService;
 
     @PostMapping("/one")
-    public void oneDownload(@RequestBody OneRequestDto dto) throws DocumentException, IOException {
+    public ResponseEntity<Object> oneDownload(@RequestBody OneRequestDto dto) throws DocumentException, IOException {
         pdfService.oneManipulate(dto);
         pdfToImgService.pdfToImg(new FileInputStream(new File("./one.pdf")), 0, "one.png");
+
+        Path filePath = Paths.get("./one.png");
+        Resource resource = new InputStreamResource(Files.newInputStream(filePath));
+        File file = new File("./one.png");
+
+        HttpHeaders headers = new HttpHeaders();
+        String contentType = Files.probeContentType(filePath);
+
+        // 다운로드 되거나 로컬에 저장되는 용도로 쓰이는지 알려주는 헤더
+        headers.setContentDisposition(ContentDisposition.builder("attachment").filename(file.getName()).build());
+        headers.add(HttpHeaders.CONTENT_TYPE, contentType);
+        return new ResponseEntity<Object>(resource, headers, HttpStatus.OK);
     }
 
     @PostMapping("/two")
-    public void twoDownload(@RequestBody TwoRequestDto dto) throws DocumentException, IOException {
+    public ResponseEntity<Object> twoDownload(@RequestBody TwoRequestDto dto) throws DocumentException, IOException {
         pdfService.twoManipulate(dto);
         pdfToImgService.pdfToImg(new FileInputStream(new File("./two.pdf")), 1, "two.png");
+
+        Path filePath = Paths.get("./two.png");
+        Resource resource = new InputStreamResource(Files.newInputStream(filePath));
+        File file = new File("./two.png");
+
+        HttpHeaders headers = new HttpHeaders();
+        String contentType = Files.probeContentType(filePath);
+
+        // 다운로드 되거나 로컬에 저장되는 용도로 쓰이는지 알려주는 헤더
+        headers.setContentDisposition(ContentDisposition.builder("attachment").filename(file.getName()).build());
+        headers.add(HttpHeaders.CONTENT_TYPE, contentType);
+        return new ResponseEntity<Object>(resource, headers, HttpStatus.OK);
     }
 }
